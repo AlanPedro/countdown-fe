62b67d17a7fe0764eba8c4796bb41e2446a76cc1
alan.hutcheson <alanhutcheson@deloitte.co.uk>
2019-02-05 13:59:11 +0000

    [AH|DM] Layout and websocket pseudo stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coverflow from 'react-coverflow';
+//import openSocket from 'socket.io-client';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      teams: [],
+      //socket: openSocket('ws://demos.kaazing.com/echo'),
+      time: 60
+    }
+  }
+
+  fn = () => console.log("hi")
+
+  handleMessage = () => {
+  }
+
+  sendMessage = () => {
+
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Coverflow width="960" height="500"
+            displayQuantityOfSide={2}
+            navigation={true}
+            enableScroll={false}
+            clickable={true}
+            active={0}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div
+          onClick={() => this.fn()}
+          onKeyDown={() => this.fn()}
+          role="menuitem"
+          tabIndex="0"
+        >
+          <img
+            src='image/path'
+            alt='title or description'
+            style={{
+              display: 'block',
+              width: '100%',
+            }}
+          />
+        </div>
+        <img src='image/path' alt='title or description' data-action="http://andyyou.github.io/react-coverflow/"/>
+        <img src='image/path' alt='title or description' data-action="http://andyyou.github.io/react-coverflow/"/>
+      </Coverflow>
+      <button onClick={this.sendMessage}> SUBMIT </button>
+    </div>
     );
   }
 }
